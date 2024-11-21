2987babfa64ed626d9fce0abc666989cf1e00ef5
dev_apus <folorunsopraise12@gmail.com>
2024-11-21 06:06:17 +0100

    Finalized

--- contracts/NFTBase.sol
+++ contracts/NFTBase.sol
@@ -1,97 +1,63 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFTBase is ERC721Enumerable, ERC721URIStorage, Ownable {
-    uint256 public initialSupply;
-    uint256 public mintedSupply;
-    bool public initialized = false;
-    uint128 public mintFee;
-    // string private customName;
-    // string private customSymbol;
+contract NFTBase is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+    uint256 public mintFee;
+    address public nftFactory;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
 
-
-    constructor(string memory name, string memory symbol, uint256 _initialSupply/*, uint128 _mintFee*/)
-        ERC721(name, symbol)
-        Ownable(msg.sender)
+    constructor(string memory _name, string memory _symbol, uint256 _mintFee,address _owner,address _factory)
+    ERC721(_name,_symbol)
+    Ownable(_owner)
     {
-        initialSupply = _initialSupply;
-        // mintFee = _mintFee;
+        mintFee = _mintFee;
+        nftFactory = _factory;
     }
 
-    // function initialize(string memory _name, string memory _symbol, uint256 _initialSupply) external {
-    //     require(initialSupply == 0, "Already initialized");
-    //     require(!initialized, "Already initialized");
-    //     initialized = true;
-    //     initialSupply = _initialSupply;
-    //     customName = _name;
-    //     customSymbol = _symbol;
-    //     _setNameAndSymbol(_name, _symbol);
-    // }
+    function safeMint (address to,string memory uri) public payable {
+        require(msg.value >= mintFee, "Insufficient Mint Fee");
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to,tokenId);
+        _setTokenURI(tokenId, uri);
+        payable(nftFactory).transfer(address(this).balance);
+    }
 
-    function mint(address _to, string memory _tokenURI) public /*payable*/  {
-        // require(msg.value >= mintFee, "Insufficient fee for minting");
-        uint256 tokenId = mintedSupply + 1;
-        _safeMint(_to, tokenId);
-        if (bytes(_tokenURI).length > 0) {
-            _setTokenURI(tokenId, _tokenURI);
+    function safeMintCollection (address to, string[] memory uris) public payable {
+        require(msg.value >= mintFee, "Insufficient Mint Fee");
+        for (uint256 i = 0; i < uris.length; i++) 
+        {
+            uint256 tokenId = _tokenIdCounter.current();
+            _tokenIdCounter.increment();
+            _safeMint(to,tokenId);
+            _setTokenURI(tokenId, uris[i]);    
         }
-        mintedSupply += 1;
+        payable(nftFactory).transfer(address(this).balance);
     }
 
-    function batchMint(address _to, string[] memory _tokenURIs) public /*payable*/ {
-        // require(msg.value >= mintFee, "Insufficient fee for minting");
-        for (uint256 i = 0; i < _tokenURIs.length; i++) {
-            require(mintedSupply < initialSupply, "Max supply reached");
-            uint256 tokenId = mintedSupply + 1;
-            _safeMint(_to, tokenId);
-            if (bytes(_tokenURIs[i]).length > 0) {
-                _setTokenURI(tokenId, _tokenURIs[i]);
-            }
-            mintedSupply += 1;
-        }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
-
-    function setInitialSupply(uint256 _initialSupply) external onlyOwner {
-        initialSupply = _initialSupply;
-    }
-
-    
-
-    //Helper function
-    // function _setNameAndSymbol(string memory _name, string memory _symbol) internal {
-    //     assembly {
-    //         sstore(0x0, _name)
-    //         sstore(0x1, _symbol)
-    //     }
-    // }
 
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721Enumerable, ERC721URIStorage)
+        override(ERC721, ERC721URIStorage)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
-    }
-
-    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-        return super.tokenURI(tokenId);
-    }
-
-    function _update(address _to, uint256 _tokenId, address _auth)
-        internal
-        virtual
-        override(ERC721, ERC721Enumerable)
-        returns (address)
-    {
-        return super._update(_to, _tokenId, _auth);
-    }
-
-    function _increaseBalance(address _account, uint128 _value) internal virtual override(ERC721, ERC721Enumerable) {
-        return super._increaseBalance(_account, _value);
     }
 }
--- contracts/NFTFactory.sol
+++ contracts/NFTFactory.sol
@@ -2,31 +2,26 @@
 pragma solidity ^0.8.0;
 
 import {NFTBase} from "./NFTBase.sol";
-import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/interfaces/IERC721.sol";
 
 contract NFTFactory is Ownable {
     mapping(address => address[]) internal  UserNFTs;
-    uint128 public mintFee;
+    uint256 public creationFee; // Fee to deploy a new NFT contract
+    uint256 public mintFee; 
 
-    event NFTContractDeployed(address indexed user, address nftContract);
-    event MintFee(uint128 fee);
+    event NFTContractCreated(address indexed creator, address nftContract);
+    event FeeUpdated(string feeType, uint256 newFee);
 
-    constructor(uint128 _mintFee) 
-    Ownable(msg.sender) 
-    {
+    constructor(uint256 _creationFee, uint256 _mintFee) Ownable(msg.sender) {
+        creationFee = _creationFee;
         mintFee = _mintFee;
     }
 
-    function createNFTContract(string memory _name, string memory _symbol, uint256 _initialSupply)
-        external payable 
-        returns (address)
-    {
-        require(msg.value >= mintFee, "Insufficient mint fee");
+    function createNFTContract(string memory _name, string memory _symbol) public payable returns (address){
+        require(msg.value >= creationFee, "Insufficient fee to create NFT contract");
         bytes memory bytecode = abi.encodePacked(
             type(NFTBase).creationCode,
-            abi.encode(_name, _symbol, _initialSupply, mintFee)
+            abi.encode(_name, _symbol, mintFee,msg.sender,address(this))
         );
 
         address deployed;
@@ -36,136 +31,19 @@ contract NFTFactory is Ownable {
         }
 
         UserNFTs[msg.sender].push(deployed);
-
-        emit NFTContractDeployed(msg.sender, deployed);
+        emit NFTContractCreated(msg.sender,deployed);
         return deployed;
     }
 
-    function mintNFT(
-        address _nftContract,
-        address _to,
-        string memory _tokenURI
-    ) external /*payable*/ {
-        // require(msg.value >= mintFee, "Insufficient mint fee");
 
-        // Validate the caller owns the NFT contract
-        bool isOwner = false;
-        address[] memory userNFTs = UserNFTs[msg.sender];
-        for (uint256 i = 0; i < userNFTs.length; i++) {
-            if (userNFTs[i] == _nftContract) {
-                isOwner = true;
-                break;
-            }
-        }
-        require(isOwner, "Not the owner of this NFT contract");
-
-        // Call the mint function on the cloned contract
-        NFTBase(_nftContract).mint(_to,_tokenURI);
-    }
-
-    function batchMint(
-        address _nftContract,
-        address _to,
-        string[] memory _tokenURIs
-    ) external /*payable*/ {
-        // require(msg.value >= mintFee, "Insufficient mint fee");
-
-        // Validate the caller owns the NFT contract
-        bool isOwner = false;
-        address[] memory userNFTs = UserNFTs[msg.sender];
-        for (uint256 i = 0; i < userNFTs.length; i++) {
-            if (userNFTs[i] == _nftContract) {
-                isOwner = true;
-                break;
-            }
-        }
-        require(isOwner, "Not the owner of this NFT contract");
-
-        // Call the mint function on the cloned contract
-        NFTBase(_nftContract).batchMint(_to, _tokenURIs);
-    }
-
-    function setMintFee(uint128 _mintFee) public {
-        mintFee = _mintFee;
-        emit MintFee(_mintFee);
-    }
-
-    function getUserNFT (address user) public view returns (address[] memory){
-        return UserNFTs[user];
-    }
-
-    //helper function
-    // function transferFeeToOwner (address payable _to, uint256 _amount, uint256 _percentage) internal returns(bool) {
-    //     require(_percentage >0 && _percentage <=100, "Percenteage must be between 1 and 100");
-    //     require(_amount > 0,  "No funds sent");
-    //     //Calculate ammount to send to user
-    //     uint256 amountToSend = (_amount * _percentage)/100;
-
-    //     // Transfer Calculated ammount
-    //     (bool success,) = _to.call{value:amountToSend}("");
-
-    //     require(success,"Transfer Failed");
-    //     return  success;
-    // }
-
-    function withdrawFees() external onlyOwner {
+    // Withdrawal function
+    function withdrawFees() public onlyOwner {
         payable(owner()).transfer(address(this).balance);
     }
 
-    // IERC721 utility functions
-    function balanceOf(address _nftContract, address owner) external view returns (uint256) {
-        return IERC721(_nftContract).balanceOf(owner);
+    function getUserNFT (address owner) public view returns (address[] memory){
+        return UserNFTs[owner];
     }
 
-    function ownerOf(address _nftContract, uint256 tokenId) external view returns (address) {
-        return IERC721(_nftContract).ownerOf(tokenId);
-    }
-
-    function approve(address _nftContract, address to, uint256 tokenId) external {
-        require(_isOwnerOfNFTContract(msg.sender, _nftContract), "Not authorized to approve");
-        IERC721(_nftContract).approve(to, tokenId);
-    }
-
-    function getApproved(address _nftContract, uint256 tokenId) external view returns (address) {
-        return IERC721(_nftContract).getApproved(tokenId);
-    }
-
-    function isApprovedForAll(address _nftContract, address owner, address operator) 
-        external 
-        view 
-        returns (bool) 
-    {
-        return IERC721(_nftContract).isApprovedForAll(owner, operator);
-    }
-
-    function transferFrom(
-        address _nftContract,
-        address from,
-        address to,
-        uint256 tokenId
-    ) external {
-        require(_isOwnerOfNFTContract(msg.sender, _nftContract), "Not authorized to transfer");
-        IERC721(_nftContract).transferFrom(from, to, tokenId);
-    }
-
-    function _isOwnerOfNFTContract(address user, address nftContract) internal view returns (bool) {
-        address[] memory userNFTs = UserNFTs[user];
-        for (uint256 i = 0; i < userNFTs.length; i++) {
-            if (userNFTs[i] == nftContract) {
-                return true;
-            }
-        }
-        return false;
-    }
-    function getNFTDetails(address _nftContract)
-        external
-        view  
-        returns (string memory, string memory,uint256)
-    {
-        return (
-            NFTBase(_nftContract).name(),
-            NFTBase(_nftContract).symbol(),
-            NFTBase(_nftContract).initialSupply()
-        );
-    }
+    receive() external payable {}
 }
